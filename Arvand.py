4367a19d104480120763357c28e4978d0bca2cb5
Luxannn <146860983+Luxannn@users.noreply.github.com>
2023-12-10 00:02:12 +0500

    Arvand.py

--- Arvand.py
+++ Arvand.py
@@ -12,13 +12,23 @@ encoded_columns = joblib.load('encoded_columns.joblib')
 
 st.title("Кредитный Скоринг")
 
-gender = st.selectbox("Пол:", ['Мужской', 'Женский'])
-married_status = st.selectbox("Семейное положение:", ['Оиладор', 'Чудошуда', 'Беоила', 'Бевамард (бевазан)'])
-
-nationality = st.selectbox("Национальность:", ['Точик', 'Узбек', 'Киргиз', 'Рус', 'Тотор', 'Другие', 'Украин'])
-educ = st.selectbox("Образование:", ['Миёна', 'Миёнаи махсус', 'Оли', 'Олии нопурра', 'Миёнаи нопурра', 'Аспирантура'])
+st.header("Персональная информация")
+gender = st.radio("Пол:", ['Мужской', 'Женский'])
+married_status = st.radio("Семейное положение:", ['Оиладор', 'Чудошуда', 'Беоила', 'Бевамард (бевазан)'])
+nationality = st.radio("Национальность:", ['Точик', 'Узбек', 'Киргиз', 'Рус', 'Тотор', 'Другие', 'Украин'])
+educ = st.radio("Образование:", ['Миёна', 'Миёнаи махсус', 'Оли', 'Олии нопурра', 'Миёнаи нопурра', 'Аспирантура'])
 family_size = st.number_input("Размер семьи:", min_value=0, max_value=100)
-bus_experience = st.number_input("Опыт в бизнесе:" , min_value=0, max_value=100)
+bus_experience = st.number_input("Опыт в бизнесе:", min_value=0, max_value=100)
+age = st.number_input("Возраст:", min_value=18, max_value=100, value=25)
+
+st.header("Финансовая информация")
+loan_amount = st.number_input("Сумма кредита:", min_value=0, max_value=1000000000)
+loan_term = st.number_input("Срок кредита (в месяцах):", min_value=0, max_value=1000)
+credit_history = st.number_input("Количество кредитных историй:", min_value=0, max_value=10000)
+net_profit = st.number_input("Чистая прибыль:", value=0)
+st.markdown("</div>", unsafe_allow_html=True)
+
+st.header("Детали кредита")
 filial = st.selectbox("Филиал:", ['Хучанд', 'Исфара', 'Истаравшан', 'Душанбе', 'Ч. Расулов', 'Панчакент'])
 region = st.selectbox("Регион:", ['Ашт', 'Кистакуз', 'Худжанд-Панчшанбе', 'Худжанд-Центр', 'Ифтихор', 'Оббурдон', 'Бустон','Мархамат', 'Сомгор', 'Шарк', 'Дусти',
        'Пунук', 'Уяс', 'Оппон', 'Конибодом', 'Кулканд', 'Ниёзбек',
@@ -41,12 +51,7 @@ credit_purpose = st.selectbox("Цель Кредита:", ['Животновод
        'Сушка фруктов', 'Коммерческий кредит',
        'Другие потребительские кредиты'])
 loan_purpose = st.selectbox("Назначение кредита:", ['Бизнес-кредит', 'Потребительский кредит', 'Жилищный кредит', 'Энергосберегающие проекты'])
-loan_amount = st.number_input("Сумма кредита:", min_value=0, max_value=1000000000)
-loan_term = st.number_input("Срок кредита (в месяцах):", min_value=0, max_value=1000)
 pledge = st.selectbox("Залог:", ['без залога', 'поручительство', 'недвижимость', 'движимое имущество'])
-credit_history = st.number_input("Количество кредитных историй:", min_value=0, max_value=10000)
-net_profit = st.number_input("Чистая прибыль:", value=0)
-age = st.number_input("Возраст:", min_value=18, max_value=100, value=25)
 
 input_data = pd.DataFrame({
     'Gender': [gender],
@@ -91,9 +96,9 @@ if st.button("Предсказать"):
     probability = model.predict_proba(df)[:, 0]
     st.subheader("Решение по выдаче кредита:")
     if credit_score == 0:
-        st.success("Можно выдать кредит.")
+        st.success("✅ Можно выдать кредит.")
     else:
-        st.error("Отказать в выдаче кредита.")
+        st.error("❌ Отказать в выдаче кредита.")
         requested_loan_amount = input_data['Сумма кредита'] 
         suggested_amount = 0.8 * requested_loan_amount.iloc[0]
         st.subheader("Предложенная сумма кредита:")
@@ -102,3 +107,4 @@ if st.button("Предсказать"):
 
     st.subheader("Вероятность возврата кредита:")
     st.write(f"{probability[0]*100:.2f}%")
+    st.markdown("Developed by: Muslim")
