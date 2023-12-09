d4b0b9f867f3bd4992c0b1482cfaf473e8b0e5d2
Luxannn <146860983+Luxannn@users.noreply.github.com>
2023-12-09 23:50:17 +0500

    Arvand.py

--- Arvand.py
+++ Arvand.py
@@ -3,21 +3,81 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import pickle
 import io
+import joblib
 import numpy as np
-pickle_in = open("Arvand_Gradient_Boosting_Scaled.pkl", "rb")
+from sklearn.preprocessing import LabelEncoder
+pickle_in = open("Arvand_Gradient_Boosting.pkl", "rb")
 model = pickle.load(pickle_in)
+encoded_columns = joblib.load('encoded_columns.joblib')
 
-df = pd.read_csv('arvand.csv')
+def apply_custom_style():
+    st.markdown(
+        """
+        <style>
+            .st-bf {
+                border-radius: 15px;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                padding: 20px;
+                margin-bottom: 20px;
+                background-color: #f5f5f5;
+            }
+            .st-bf header {
+                font-size: 1.5em;
+                color: #333;
+                margin-bottom: 15px;
+            }
+            .st-bf label {
+                color: #555;
+            }
+            .st-bf .stButton button {
+                background-color: #4CAF50;
+                color: white;
+                border: none;
+                padding: 10px 20px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                border-radius: 5px;
+                cursor: pointer;
+                transition-duration: 0.3s;
+            }
+            .st-bf .stButton button:hover {
+                background-color: #45a049;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+apply_custom_style()
 
 st.title("Кредитный Скоринг")
 
-gender = st.selectbox("Пол:", ['Мужской', 'Женский'])
-married_status = st.selectbox("Семейное положение:", ['Оиладор', 'Чудошуда', 'Беоила', 'Бевамард (бевазан)'])
 
-nationality = st.selectbox("Национальность:", ['Точик', 'Узбек', 'Киргиз', 'Рус', 'Тотор', 'Другие', 'Украин'])
-educ = st.selectbox("Образование:", ['Миёна', 'Миёнаи махсус', 'Оли', 'Олии нопурра', 'Миёнаи нопурра', 'Аспирантура'])
+st.markdown("<div class='st-bf'>", unsafe_allow_html=True)
+st.header("Персональная информация")
+gender = st.radio("Пол:", ['Мужской', 'Женский'])
+married_status = st.radio("Семейное положение:", ['Оиладор', 'Чудошуда', 'Беоила', 'Бевамард (бевазан)'])
+nationality = st.radio("Национальность:", ['Точик', 'Узбек', 'Киргиз', 'Рус', 'Тотор', 'Другие', 'Украин'])
+educ = st.radio("Образование:", ['Миёна', 'Миёнаи махсус', 'Оли', 'Олии нопурра', 'Миёнаи нопурра', 'Аспирантура'])
 family_size = st.number_input("Размер семьи:", min_value=0, max_value=100)
-bus_experience = st.number_input("Опыт в бизнесе:" , min_value=0, max_value=100)
+bus_experience = st.number_input("Опыт в бизнесе:", min_value=0, max_value=100)
+age = st.number_input("Возраст:", min_value=18, max_value=100, value=25)
+st.markdown("</div>", unsafe_allow_html=True)
+
+st.markdown("<div class='st-bf'>", unsafe_allow_html=True)
+st.header("Финансовая информация")
+loan_amount = st.number_input("Сумма кредита:", min_value=0, max_value=1000000000)
+loan_term = st.number_input("Срок кредита (в месяцах):", min_value=0, max_value=1000)
+credit_history = st.number_input("Количество кредитных историй:", min_value=0, max_value=10000)
+net_profit = st.number_input("Чистая прибыль:", value=0)
+st.markdown("</div>", unsafe_allow_html=True)
+
+
+st.markdown("<div class='st-bf'>", unsafe_allow_html=True)
+st.header("Детали кредита")
 filial = st.selectbox("Филиал:", ['Хучанд', 'Исфара', 'Истаравшан', 'Душанбе', 'Ч. Расулов', 'Панчакент'])
 region = st.selectbox("Регион:", ['Ашт', 'Кистакуз', 'Худжанд-Панчшанбе', 'Худжанд-Центр', 'Ифтихор', 'Оббурдон', 'Бустон','Мархамат', 'Сомгор', 'Шарк', 'Дусти',
        'Пунук', 'Уяс', 'Оппон', 'Конибодом', 'Кулканд', 'Ниёзбек',
@@ -40,13 +100,10 @@ credit_purpose = st.selectbox("Цель Кредита:", ['Животновод
        'Сушка фруктов', 'Коммерческий кредит',
        'Другие потребительские кредиты'])
 loan_purpose = st.selectbox("Назначение кредита:", ['Бизнес-кредит', 'Потребительский кредит', 'Жилищный кредит', 'Энергосберегающие проекты'])
-loan_amount = st.number_input("Сумма кредита:", min_value=0, max_value=1000000000)
-loan_term = st.number_input("Срок кредита (в месяцах):", min_value=0, max_value=1000)
 pledge = st.selectbox("Залог:", ['без залога', 'поручительство', 'недвижимость', 'движимое имущество'])
-credit_history = st.number_input("Количество кредитных историй:", min_value=0, max_value=10000)
-net_profit = st.number_input("Чистая прибыль:", value=0)
-age = st.number_input("Возраст:", min_value=18, max_value=100, value=25)
+st.markdown("</div>", unsafe_allow_html=True)
 
+     
 input_data = pd.DataFrame({
     'Gender': [gender],
     'FamilySize': [family_size],
@@ -66,42 +123,42 @@ input_data = pd.DataFrame({
     'Назначение': [loan_purpose],
     'Залог': [pledge]  
 })
-s = str()
-dp = pd.DataFrame()
-input_data['Gender'] = input_data['Gender'].map({'Мужской': 0, 'Женский': 1})
-for i in input_data:
-    s += i 
-    s += '_'
-    for j in input_data[i]:
-        s += str(j)
-    dp[s] = 0;
-    s = str()
-    
-for i in dp:
-    for j in df:
-        if i == j:
-            df[j] = np.uint8(1)
-        else:
-            df[j] = np.uint8(0)
-    
-df_num = input_data.select_dtypes(exclude=['object'])
 
-for i in df_num:
-    for j in df:
-        if i == j:
-            df[j] = df_num[i]
-     
+if st.button("Предсказать",key="predict_button"):
     
+    input_data['Gender'] = input_data['Gender'].map({'Мужской': 0, 'Женский': 1})
 
-credit_score = model.predict(df)[0]
-if st.button("Предсказать"):
+    input_data['Gender'] = input_data['Gender'].astype(int)
+    
+    df_cat = input_data.select_dtypes(include=['object'])
+    df_num = input_data.select_dtypes(exclude=['object'])
+    df_encoded = pd.get_dummies(df_cat , dtype=np.uint8 )
 
-    probability = model.predict_proba(df)[:, 1]
+    for col in encoded_columns:
+        if col not in df_encoded.columns:
+            df_encoded[col] = np.uint8(0)
+
+    df_encoded = df_encoded[encoded_columns]    
+    df = pd.concat([df_num, df_encoded], axis = True)
+        
+    #st.dataframe(df)
+    credit_score = model.predict(df)[0]
+    
+    probability = model.predict_proba(df)[:, 0]
+    st.markdown("<div class='st-bf'>", unsafe_allow_html=True)
     st.subheader("Решение по выдаче кредита:")
-    if probability >= 55:
-        st.success("Можно выдать кредит.")
+
+    if credit_score == 0:
+        st.success("✅ Можно выдать кредит.")
+        #st.balloons()
     else:
-        st.error("Отказать в выдаче кредита.")
+        st.error("❌ Отказать в выдаче кредита.")
+        requested_loan_amount = input_data['Сумма кредита']
+        suggested_amount = 0.8 * requested_loan_amount.iloc[0]
+        st.subheader("Предложенная сумма кредита:")
+        st.write(f"{suggested_amount:.2f} сомони.")
 
     st.subheader("Вероятность возврата кредита:")
     st.write(f"{probability[0]*100:.2f}%")
+    st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown("Developed by: Muslim")
